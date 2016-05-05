e13fbc7b7a7e4c0181b6393a3460e3dfeac130ac
zhouxiaotian <1282347298@qq.com>
2016-05-05 13:23:50 +0800

    Ajax基础3

--- 20160505/server.js
+++ 20160505/server.js
@@ -2,6 +2,12 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs");
 
+var getInfo = require("./module/getInfo");
+var addInfo = require("./module/addInfo");
+var updateInfo = require("./module/updateInfo");
+var deleteInfo = require("./module/deleteInfo");
+var detailInfo = require("./module/detailInfo");
+
 //->使用NODE创建一个服务:当客户端发送一次请求的时候就会把对应的回调函数执行,并且传递两个参数值REQUEST、RESPONSE
 var server = http.createServer(function (req, res) {
     //->解析客户端发送请求的这个地址
@@ -16,6 +22,35 @@ var server = http.createServer(function (req, res) {
         var conType = suffix === "html" ? "text/html" : (suffix === "css" ? "text/css" : "text/javascript");
         res.writeHead(200, {'content-type': conType});
         res.end(fileCon);
+        return;
+    }
+
+    //->获取全部客户信息的接口
+    if (pathname === "/getInfo") {
+        getInfo.init(res);
+        return;
+    }
+
+    //->增加用户信息的接口
+    if (pathname === "/addInfo") {
+        addInfo.init(query, res);
+        return;
+    }
+
+    //->增加用户信息的接口
+    if (pathname === "/updateInfo") {
+        updateInfo.init(query, res);
+        return;
+    }
+
+    //->删除用户信息的接口
+    if (pathname === "/deleteInfo") {
+        deleteInfo.init(query, res);
+    }
+
+    //->获取详细信息的接口
+    if (pathname === "/detailInfo") {
+        detailInfo.init(query, res);
     }
 });
 server.listen(9876, function () {
